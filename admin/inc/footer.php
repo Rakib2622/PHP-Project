43694358d83f124456cd6f951b2d302ed6ad9e2f
Rakib2622 <60670376+Rakib2622@users.noreply.github.com>
2022-10-31 02:09:43 +0600

    Add files via upload

---
+++ admin/inc/footer.php
@@ -0,0 +1,12 @@
+<div class="clear">
+        </div>
+    </div>
+    <div class="clear">
+    </div>
+    <div id="site_info">
+        <p>
+         &copy; Copyright <a href="https://www.linkedin.com/in/rakibul-islam-43b711219/">Rakibul Islam</a>. All Rights Reserved.
+        </p>
+    </div>
+</body>
+</html>
